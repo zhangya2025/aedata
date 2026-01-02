680068df0c31d82cfcc0b9fa77433bf3feda5bb2
Wind <zhangya2023@gmail.com>
2026-01-02 23:15:05 +0800

    feat(mu-plugins): add site stability lock

---
+++ wp-content/themes/aegis-themes/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Theme setup for Aegis-Themes.
+ */
+
+define( 'AEGIS_THEMES_VERSION', '0.1.0' );
+
+add_action( 'after_setup_theme', function () {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'responsive-embeds' );
+    add_theme_support( 'editor-styles' );
+
+    add_editor_style( 'assets/css/main.css' );
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'aegis-themes-style', get_theme_file_uri( 'assets/css/main.css' ), array(), AEGIS_THEMES_VERSION );
+    wp_enqueue_script( 'aegis-themes-script', get_theme_file_uri( 'assets/js/main.js' ), array(), AEGIS_THEMES_VERSION, true );
+} );
---
+++ wp-content/themes/aegis-themes/readme.txt
@@ -0,0 +1,8 @@
+Aegis-Themes
+============
+
+A lightweight block theme skeleton for WordPress full site editing.
+
+Version: 0.1.0
+Author: Aegis
+License: GPL-2.0-or-later
