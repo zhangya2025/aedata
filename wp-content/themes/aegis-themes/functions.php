844fa628ba3f5f264accb6e879f8d4ee5a1fa43f
Wind <zhangya2023@gmail.com>
2026-01-05 09:12:09 +0800

    Apply Noto Sans SC to mega header

--- wp-content/themes/aegis-themes/functions.php
+++ wp-content/themes/aegis-themes/functions.php
@@ -15,6 +15,10 @@ add_action( 'after_setup_theme', function () {
 } );
 
 add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style( 'aegis-font-noto-sans-sc', get_stylesheet_directory_uri() . '/assets/fonts/noto-sans-sc/noto-sans-sc.css', array(), AEGIS_THEMES_VERSION );
+}, 5 );
+
+add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'aegis-themes-style', get_theme_file_uri( 'assets/css/main.css' ), array(), AEGIS_THEMES_VERSION );
     wp_enqueue_script( 'aegis-themes-script', get_theme_file_uri( 'assets/js/main.js' ), array(), AEGIS_THEMES_VERSION, true );
 } );
